ecc2a18de7aba5ee7bfe836e7eda0e12a48ff6c1
Cody Duskin <codyduskin@me.com>
2017-10-23 12:07:31 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <br></br>
+          <br></br>
+          <br></br>
+          <h1 className="App-title">Welcome to bxShr</h1>
+        </header>
+        <p className="App-intro">
+          App coming soon...
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
